454a674bc3c700a5c72e82d9a6cbe2f952a1c4e3
voste126 <steveaustine126@gmail.com>
2025-03-25 19:42:05 +0300

    FIX: Build configurations

--- expense-tracker/vite.config.ts
+++ expense-tracker/vite.config.ts
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Listen on all interfaces
     host: '0.0.0.0',
-    // Use port 8080 for OpenShift/Knative
     port: 8080,
-    // Permit requests from the specified hosts
-    allowedHosts: ['kazi-chap-chap-sa-3118-dev.apps.rm2.thpm.p1.openshiftapps.com']
-
+    allowedHosts: ['.apps.rm2.thpm.p1.openshiftapps.com']
   }
 })
+
 
